b6a461cab2420b308ea4b066420f4fb1f68dae15
Alberto Bettini <alberto.bettini@marconirovereto.it>
2022-07-28 16:13:08 +0200

    prove tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {  
+  theme: {
+      extend: {
+          colors: {
+              primary: '#EBB012',
+              // "primary-dark": defaultTheme.colors.purple,
+              secondary: '#F68107',
+              // "secondary-dark": defaultTheme.colors.black,
+              // text: defaultTheme.colors.black,
+              // "text-dark": defaultTheme.colors.white,
+              // error: defaultTheme.colors.green,
+              // "error-dark": defaultTheme.colors.green,
+              // info: defaultTheme.colors.green,
+              // "info-dark": defaultTheme.colors.green,
+              // success: defaultTheme.colors.green,
+              // "success-dark": defaultTheme.colors.green,
+              // warning: defaultTheme.colors.green,
+              // "warning-dark": defaultTheme.colors.green,
+          }
+      },
+      backgroundImage: {
+          'bgheader': "url('img/7.JPG')",
+      },
+  }
+}
+
